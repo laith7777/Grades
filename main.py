e924fbc377089289690de223cfbde9b12abbb6f5
laith7777 <lbh5303@psu.edu>
2020-09-10 18:12:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+'''
+Author: Yanling Wang yuw17@psu.edu
+Collaborator: Laith Hayajneh
+Section: 1
+Breakout: 12
+'''
+
+n = float(input("Enter your CMPSC 131 grade:"))
+
+if n >= 93.0:
+  print ("Your letter grade for CMPSC 131 is A.")
+elif n >= 90.0:
+  print ("Your letter grade for CMPSC 131 is A-.")
+elif n >= 87.0:
+  print ("Your letter grade for CMPSC 131 is B+.") 
+elif n >= 83.0:
+  print ("Your letter grade for CMPSC 131 is B.")
+elif n >= 80.0:
+  print ("Your letter grade for CMPSC 131 is B-.")
+elif n >= 77.0:
+  print ("Your letter grade for CMPSC 131 is C+.")
+elif n >= 70.0:
+  print ("Your letter grade for CMPSC 131 is C.")
+elif n >= 60.0:
+  print ("Your letter grade for CMPSC 131 is D.")
+else:
+  print ("Your letter grade for CMPSC 131 is F.")
