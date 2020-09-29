f0e289be2bf54a105e6896ecb9537dd10481b62a
laith7777 <lbh5303@psu.edu>
2020-09-29 18:25:10 +0000

    course 1

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 
 # Author: Laith Hayajneh lbh5303@psu.edu
 
-n = float(input("Enter your CMPSC 131 grade:"))
+n = float(input("Enter your course 1 grade:"))
 
 if n >= 93.0:
-  print ("Your letter grade for CMPSC 131 is A.")
+  print ("Your letter grade for course 1 is A.")
 elif n >= 90.0:
-  print ("Your letter grade for CMPSC 131 is A-.")
+  print ("Your letter grade for course 1 is A-.")
 elif n >= 87.0:
-  print ("Your letter grade for CMPSC 131 is B+.") 
+  print ("Your letter grade for course 1 is B+.") 
 elif n >= 83.0:
-  print ("Your letter grade for CMPSC 131 is B.")
+  print ("Your letter grade for course 1 is B.")
 elif n >= 80.0:
-  print ("Your letter grade for CMPSC 131 is B-.")
+  print ("Your letter grade for course 1 is B-.")
 elif n >= 77.0:
-  print ("Your letter grade for CMPSC 131 is C+.")
+  print ("Your letter grade for course 1 is C+.")
 elif n >= 70.0:
-  print ("Your letter grade for CMPSC 131 is C.")
+  print ("Your letter grade for course 1 is C.")
 elif n >= 60.0:
-  print ("Your letter grade for CMPSC 131 is D.")
+  print ("Your letter grade for course 1 is D.")
 else:
-  print ("Your letter grade for CMPSC 131 is F.")
+  print ("Your letter grade for course 1 is F.")
